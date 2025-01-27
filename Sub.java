bf0f39d339e51ee60d71b68556ae83d294c47beb
pranam244 <sqlactivitycs@gmail.com>
2025-01-27 14:55:07 +0530

    Create Sub.java
    answer

---
+++ Sub.java
@@ -0,0 +1,11 @@
+class Sub
+  {
+    public static void main(String args[])
+    {
+      int a,b,sub;
+      a=8;
+      b=3;
+      sub=a-b;
+      System.out.println("Subtract of two number is :"+sub);
+    }
+  }
